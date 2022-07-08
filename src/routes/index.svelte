9f9efa469ebba4d46ac138fd7ce0466e7485f1b2
Davey Chu <davey.chu@ease-software.nl>
2022-07-08 16:21:06 +0200

    Reproduce onDestroy not called after goto

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,26 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import { goto } from "$app/navigation";
+  import { onDestroy } from "svelte";
+  import { fly } from "svelte/transition";
+  import { store } from "../store";
+
+  async function click() {
+    setTimeout(() => {
+      console.log("Set show to true");
+      $store.show = true;
+    }, 1000);
+    await goto("other");
+  }
+
+  onDestroy(() => {
+    console.log("On destroy called");
+  });
+</script>
+
+<h1>Page 1</h1>
+
+<button on:click={click}>Go to page 2</button>
+
+{#if $store.show}
+  <p transition:fly={{ y: 100, duration: 2000 }}>Peekaboo!</p>
+{/if}
---
+++ src/routes/other.svelte
@@ -0,0 +1,7 @@
+<script>
+  import { goto } from "$app/navigation";
+</script>
+
+<h1>Page 2</h1>
+
+<button on:click={() => goto("/")}>Go to page 1</button>
